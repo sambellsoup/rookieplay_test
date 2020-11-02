733a1398f268a709f1de2df89cacd1e658d8610e
Sam Bell <sambellsoup@gmail.com>
2020-11-02 06:49:06 -0500

    added document upload model

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -18,4 +19,9 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path('', include('hello.urls')),
 ]
+
+# Only for development purposes.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Document(models.Model):
+    """Document uploaded by user to be analyzed"""
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,)
+    created_at = models.DateTimeField(default=date.today)
+    title = models.CharField(max_length=100)
+    document = models.FileField(null=True)
+
+    def __str__(self):
+        return self.title
